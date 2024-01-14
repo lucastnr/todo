4d379e9bafbfd2590ab56c8c2bced6c72319fdc1
Lucas Tenorio <tnrlucas@gmail.com>
2024-01-14 18:27:41 -0300

    Remove Auth0 issuer from env

--- src/env.js
+++ src/env.js
@@ -30,7 +30,6 @@ export const env = createEnv({
     ),
     AUTH0_CLIENT_ID: z.string().min(1),
     AUTH0_CLIENT_SECRET: z.string(),
-    AUTH0_ISSUER: z.string(),
   },
 
   /**
@@ -53,7 +52,6 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_ISSUER: process.env.AUTH0_ISSUER,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
